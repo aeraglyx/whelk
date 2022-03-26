3bfaa6dcabffadb17cb4a54876aadac29466bc0e
Vladislav Macíček <vladislav.cam24@gmail.com>
2022-03-26 16:33:34 +0100

    stuff

--- main.jl
+++ main.jl
@@ -13,95 +13,85 @@ end
 
 
 
-const SFB_PENALTY  = 4.0::Float64
-const INWARD_ROLL  = 0.7::Float64
-const OUTWARD_ROLL = 1.2::Float64
+# const SFB_PENALTY  = 4.0::Float64
+# const INWARD_ROLL  = 0.7::Float64
+# const OUTWARD_ROLL = 1.2::Float64
 
-function analyze(dict, texts)
+function analyze(dict, data)
+
+	SFB_PENALTY ::Float64 = 4.0
+	INWARD_ROLL ::Float64 = 0.7
+	OUTWARD_ROLL::Float64 = 1.2
 	
-	key_prev = false
-	same_hand_streak::Int64 = 0
 	score::Float64 = 0.0
-
-	char_count::Int64 = 0
+	
 	# sfb_count = 0
 	# roll_count = 0
 	# left_hand_count = 0
-
-	# TODO simd cant deal with "continue"
-
-	for text in texts
-		open(text, "r") do file
+		
+	for entry in data
+		
+		word, freq = entry
+		word_score::Float64 = 0.0
+		key_prev = false
+		same_hand_streak::Int64 = 0
+		# print(file)
+		for char in word  # TODO function for analyzing a single word
+		# while !eof(file)
+			# char = lowercase(read(file, Char))
+			# char_count += 1
+			if !haskey(dict, char)  # TODO make sure there are already no illegal chars
+				key_prev = false
+				# same_hand_streak += 0.5
+				# left_hand_count += 0.5
+				continue
+			end
 			
-			# print(file)
-			while !eof(file)
-				char = lowercase(read(file, Char))
-				char_count += 1
-				if !haskey(dict, char)
-					key_prev = false
-					# same_hand_streak += 0.5
-					# left_hand_count += 0.5
-					continue
-				end
-				
-				key = dict[char]
-				
-				if key_prev == false
-					key_prev = key
-					continue
-				end
+			key = dict[char]
 						
-				# finger = abs(key[1])::Int64
-				
-				stroke_effort::Float64 = 0.0
-				
-				if key.finger == 1
-					stroke_effort = 1.2
-				elseif key.finger == 2
-					stroke_effort = 1.0
-				elseif key.finger == 3
-					stroke_effort = 1.5
-				elseif key.finger == 4
-					stroke_effort = 2.3
-				end
-				
-				if key.row != 2
-					@fastmath stroke_effort *= 1.5
-				end
-				
+			stroke_effort::Float64 = 0.0
+			
+			if key.finger == 1
+				stroke_effort = 1.2
+			elseif key.finger == 2
+				stroke_effort = 1.0
+			elseif key.finger == 3
+				stroke_effort = 1.5
+			elseif key.finger == 4
+				stroke_effort = 2.3
+			end
+			
+			if key.row != 2
+				stroke_effort *= 1.5
+			end
+			
+			if key_prev != false
 				if key.hand == key_prev.hand
-					@fastmath stroke_effort *= 1 + same_hand_streak * 0.25
+					stroke_effort *= 1 + same_hand_streak * 0.25
 					same_hand_streak += 1
-					
 					if key.finger == key_prev.finger
-						# SFB
-						# @fastmath sfb_count += 1
-						@fastmath stroke_effort *= SFB_PENALTY
-						
+						stroke_effort *= SFB_PENALTY
+						# sfb_count += 1
 					elseif key.finger < key_prev.finger
-						# inward roll
-						# @fastmath roll_streak += 1
-						# @fastmath roll_count += 1
-						@fastmath stroke_effort *= INWARD_ROLL
+						stroke_effort *= INWARD_ROLL
+						# roll_count += 1
 					elseif key.finger > key_prev.finger
-						# outward roll
-						# @fastmath roll_streak += 1
-						# @fastmath roll_count += 1
-						@fastmath stroke_effort *= OUTWARD_ROLL
+						stroke_effort *= OUTWARD_ROLL
+						# roll_count += 1
 					end
-					
-					# @fastmath travel = key.row - key_prev.row
-					@fastmath travel = 1.0 + (key.row - key_prev.row) * 0.5
-					@fastmath stroke_effort *= travel
+					travel = 1.0 + (key.row - key_prev.row) * 0.5
+					stroke_effort *= travel  # TODO assume hand starts at home row
 				else
 					same_hand_streak = 1
 				end
-				key_prev = key
-				@fastmath score += stroke_effort
 			end
+
+			key_prev = key
+			word_score += stroke_effort
 		end
-		# println(score / char_count)
+		score += word_score * freq / length(word)  # TODO loop i for chars for faster len ?
 	end
+	println(score)
 end
 
 function prepare_corpus()
@@ -145,19 +135,21 @@ end
 using DelimitedFiles
 
 
-function test(freq_file)
-	a = Vector{Tuple}()
+function prep_freq_data(freq_file::String, n::Int)
+	data = Vector{Tuple{String, Int64}}(undef, n)
+	freq_total::Int = 0
 	open(freq_file, "r") do file
-		for i in 1:16
-			x = split(readline(file), " ")
-			push!(a, (string(x[1]), parse(Int, x[2])))
+		for i in 1:n
+			word, freq = split(readline(file), " ")
+			freq = parse(Int, freq)
+			freq_total += freq
+			data[i] = (string(word), freq)
 			# println(line)
 		end
 	end
-	display(a)
-	# x = readdlm(freq_file)
-	# print(x)
-	return a
+	data = [(word, freq / freq_total) for (word, freq) in data]
+	# display(data)
+	return data
 end
 	
 function main()
@@ -179,15 +171,15 @@ function main()
 		end
 	end
 
-	display(char_key_dict)
+	# display(char_key_dict)
 
 	# corpus_file = "corpus.txt"
-	texts = readdir("texts", join=true)
-	word_freq = "en_50k.txt"
-	@time analyze(char_key_dict, texts)
+	# texts = readdir("texts", join=true)
+	# word_freq = "en_50k.txt"
+	data = prep_freq_data("en_50k.txt", 8192)
+	# print(data)
+	@time analyze(char_key_dict, data)
 
 end
 
-# main()
-
-test("en_50k.txt")
+main()

c540e7dc1a3c63057e7fefcd66adb314631b3498
Vladislav Macíček <vladislav.cam24@gmail.com>
2022-04-02 04:32:55 +0200

    stuff

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # Whelk
 
-Nothing to see here, still WIP. Just a little experiment to generate a decent 8x3 keyboard layout.
+Nothing to see here, still WIP. Just a little experiment to generate a decent 8x3 keyboard layout based on language preferences.
 
 Initial layout the program found (not final):
 ```
-c w h m  g u p j
-s r n t  a e i o
-b v l d  f z y k
+b f r m  u o p j
+w s n t  i e h g
+v c l d  y a k z
 ```
+
+## Why 8x3
+
+Lateral motion is bad, stretching and curling is fine. So why not have 3 keys per each finger? On 36 key layouts there are 6 bad keys (inner columns), and 4 rogue punctuation marks. If we keep only letters on the main block, there will be 2 letters that won't fit. That's not a problem however since on a layout this small you'd utilize layers heavily anyway. Here's the form factor I'm working with:
+```
+□ □ □ □       □ □ □ □
+■ ■ ■ ■       ■ ■ ■ ■
+□ □ □ □       □ □ □ □
+	□ ■ □   □ ■ □
+```
+
+
+
+## Goal
+
+I wanted an idealized simplistic 
+
+---
 
 > We don’t stand a whelk’s chance in a supernova.
--- main.jl
+++ main.jl
@@ -10,11 +10,15 @@ struct Key
 end
 
 mutable struct Layout
-	char_key_dict::Dict{Char, Key}  # TODO vector of pairs ?
+	layout_chars::Vector{Char}
 	score::Float64
+	finger_load::Vector{Float64}
 end
 
 
+
+
+# naka_rushton(x, p, g) = pow(x / p, g) / (pow(x / p, g) + 1)
 function naka_rushton(x::Float64, p::Float64, g::Float64)::Float64
 	tmp = (x / p) ^ g
 	return tmp / (tmp + 1.0)
@@ -24,7 +28,7 @@ function discard_bad_layouts!(layouts, pivot::Float64, gamma::Float64)
 	# new = [layout for (i, layout) in enumerate(layouts) if naka_rushton(convert(Float64, i), pivot, gamma) < rand(Float64)]
 	new = Vector{Layout}()
 	for (i, layout) in enumerate(layouts)
-		if naka_rushton(convert(Float64, i), pivot, gamma) < rand(Float64)
+		if naka_rushton(convert(Float64, i) - 1.0, pivot, gamma) < rand(Float64)
 			push!(new, layout)
 		# else
 		end
@@ -33,24 +37,36 @@ function discard_bad_layouts!(layouts, pivot::Float64, gamma::Float64)
 	return new
 end
 
-# function naka_rushton(x, p, g) = pow(x / p, g) / (pow(x / p, g) + 1)
-
 
 function print_layout(layout::Layout)
-	println("something")  # TODO 
-	dict = layout.char_key_dict
-	collect(keys(dict))
+	chars = layout.layout_chars
+	println(join(chars[1:4],   ' ') * "  " * join(chars[5:8],   ' '))
+	println(join(chars[9:12],  ' ') * "  " * join(chars[13:16], ' '))
+	println(join(chars[17:20], ' ') * "  " * join(chars[21:24], ' '))
 end
 
 # using StatsBase
-function swap_keys!(layout::Layout)
-	dict = layout.char_key_dict
-	keysx = collect(keys(dict))
-	# rnd = StatsBase.sample(1:24, 2, replace=false)  # TODO 
-	rnd1 = rand(1:24)
-	rnd2 = rand(1:24)
-	# println(rnd1)
-	dict[keysx[rnd1]], dict[keysx[rnd2]] = dict[keysx[rnd2]], dict[keysx[rnd1]]
+function swap_keys!(layout::Layout, freq_keys_on_home_row::Bool)
+	vec = layout.layout_chars
+	n::Int = rand(1:8)
+	if freq_keys_on_home_row
+		for _ in 1:n
+			rnd1 = rand([1:8; 17:24])
+			rnd2 = rand([1:8; 17:24])
+			vec[rnd1], vec[rnd2] = vec[rnd2], vec[rnd1]
+		end
+		for _ in 1:round(3.0 * rand(Float64))
+			rnd1 = rand([9:16])
+			rnd2 = rand([9:16])
+			vec[rnd1], vec[rnd2] = vec[rnd2], vec[rnd1]
+		end
+	else
+		for _ in 1:n
+			rnd1 = rand(1:24)
+			rnd2 = rand(1:24)
+			vec[rnd1], vec[rnd2] = vec[rnd2], vec[rnd1]
+		end
+	end
 end
 
 function get_word_data(lang_prefs::Dict{String, Float64}, n::Int)::Dict{String, Float64}
@@ -64,6 +80,7 @@ function get_word_data(lang_prefs::Dict{String, Float64}, n::Int)::Dict{String, 
 				word, freq = split(readline(file), ' ')
 				word = normalize(string(word), stripmark=true, casefold=true)
 				word = filter(isletter, word)
+				# println(word)
 				freq = parse(UInt, freq)
 				freq_total += freq
 				data[i] = (word, freq)
@@ -91,26 +108,25 @@ function get_ngram_data(word_freq_data::Dict{String, Float64}, n::Int)
 				bigram_data[ngram] = freq
 			end
 		end
+	end
 	freq_total = sum(values(bigram_data))
 	bigram_data = Dict(ngram => freq / freq_total for (ngram, freq) in bigram_data)
-	end
 	return bigram_data
 end
 
-function analyze_letter(char::SubString, char_key_dict)::Float64
+function analyze_letter(char::SubString, char_key_dict, key_objects, settings)::Float64
 	if only(char) == 'x' || only(char) == 'q'
 		return 0.0
 	end
-	key::Key = char_key_dict[only(char)]
-	finger_strengths = (1.2, 1.0, 1.5, 2.3)
-	stroke_effort::Float64 = finger_strengths[key.finger]
+	key::Key = key_objects[char_key_dict[only(char)]]
+	stroke_effort::Float64 = settings.finger_efforts[key.finger]
 	if key.row != 2
-		stroke_effort *= 1.5
+		stroke_effort *= settings.not_home_row
 	end
 	return stroke_effort
 end
 
-function analyze_bigram(bigram::SubString, char_key_dict)
+function analyze_bigram(bigram::SubString, char_key_dict, key_objects, settings)::Float64
 
 	if only(bigram[1]) == 'x' || only(bigram[1]) == 'q' || only(bigram[2]) == 'x' || only(bigram[2]) == 'q'
 		return 0.0
@@ -118,112 +134,67 @@ function analyze_bigram(bigram::SubString, char_key_dict)
 
 	# TODO precompute key-key pairs ?
 	
-	SFB_PENALTY ::Float64 = 4.0
-	INWARD_ROLL ::Float64 = 0.7
-	OUTWARD_ROLL::Float64 = 1.2
+	key_1::Key = key_objects[char_key_dict[bigram[1]]]
+	key_2::Key = key_objects[char_key_dict[bigram[2]]]
 	
-	key_1::Key = char_key_dict[bigram[1]]
-	key_2::Key = char_key_dict[bigram[2]]
-	
-	finger_strengths = (1.2, 1.0, 1.5, 2.3)
-	trans_effort::Float64 = (finger_strengths[key_1.finger] + finger_strengths[key_2.finger]) * 0.5
+	trans_effort::Float64 = (settings.finger_efforts[key_1.finger] + settings.finger_efforts[key_2.finger]) * 0.5
 
 	if key_1.hand == key_2.hand
 		if key_1.finger == key_2.finger
-			trans_effort *= SFB_PENALTY
-		elseif key_1.finger < key_2.finger
-			trans_effort *= INWARD_ROLL
+			trans_effort *= settings.sfb
 		elseif key_1.finger > key_2.finger
-			trans_effort *= OUTWARD_ROLL
+			trans_effort *= settings.inroll
+		elseif key_1.finger < key_2.finger
+			trans_effort *= settings.outroll
 		end
-		travel = 1.0 + (key_1.row - key_2.row) * 0.5
+		travel = 1.0 + abs(key_1.row - key_2.row) * 0.5
 		trans_effort *= travel
+	else
+		trans_effort *= settings.alter
 	end
 	return trans_effort
 end
 
-
-
-function analyze_ngrams(ngram_data, analyze_ngram, char_key_dict)::Float64
+function analyze_ngrams(ngram_data, analyze_ngram, char_key_dict, key_objects, settings)::Float64
 	score::Float64 = 0.0
 	for (ngram, freq) in ngram_data
-		ngram_score = analyze_ngram(ngram, char_key_dict)
+		ngram_score = analyze_ngram(ngram, char_key_dict, key_objects, settings)
 		score += ngram_score * freq
 	end
 	return score
 end
 
-function analyze_word(word::String, dict::Dict{Char, Key})::Float64
-
-	SFB_PENALTY ::Float64 = 4.0
-	INWARD_ROLL ::Float64 = 0.7
-	OUTWARD_ROLL::Float64 = 1.2
-
-	score::Float64 = 0.0
-	key_prev = false
-	same_hand_streak::Int64 = 0
-
-	for char in word
-
-		if !haskey(dict, char)
-			key_prev = false
-			continue
-		end
-		
-		key = dict[char]
-		
-		finger_strengths = (1.2, 1.0, 1.5, 2.3)
-		stroke_effort::Float64 = 0.0
-		stroke_effort = finger_strengths[key.finger]
-		
-		if key.row != 2
-			stroke_effort *= 1.5
-		end
-		
-		if key_prev != false
-			if key.hand == key_prev.hand
-				stroke_effort *= 1 + same_hand_streak * 0.25
-				same_hand_streak += 1
-				if key.finger == key_prev.finger
-					stroke_effort *= SFB_PENALTY
-					# sfb_count += 1
-				elseif key.finger < key_prev.finger
-					stroke_effort *= INWARD_ROLL
-					# roll_count += 1
-				elseif key.finger > key_prev.finger
-					stroke_effort *= OUTWARD_ROLL
-					# roll_count += 1
-				# TODO redirects
-				end
-				travel = 1.0 + (key.row - key_prev.row) * 0.5
-				stroke_effort *= travel  # TODO assume hand starts at home row
-			else
-				same_hand_streak = 1
-			end
-		end
-		key_prev = key
-		score += stroke_effort
+function get_finger_load(char_key_dict, letter_data, key_objects)
+	f = Vector{Float64}(undef, 8)
+	for (char, key_idx) in char_key_dict
+		key = key_objects[key_idx]
+		f[key.hand ? 4 + key.finger : 5 - key.finger] += letter_data[string(char)]
 	end
-	return score / length(word)
+	print(f)
+	return f
 end
 
+function make_char_dict(chars)
+	char_key_dict::Dict{Char, Int} = Dict(chars[i] => i for i in 1:24)
+	return char_key_dict
+end
 
-
-function analyze_layout(layout, letter_data, bigram_data)
-	char_key_dict = layout.char_key_dict
-	score_letters = analyze_ngrams(letter_data, analyze_letter, char_key_dict)
-	score_bigrams = analyze_ngrams(bigram_data, analyze_bigram, char_key_dict)
-	score::Float64 = score_letters * 0.5 + score_bigrams * 0.5
+function analyze_layout(layout, letter_data, bigram_data, key_objects::Tuple, settings)::Float64
+	char_key_dict = make_char_dict(layout.layout_chars)
+	# finger_load = get_finger_load(char_key_dict, letter_data, key_objects)
+	
+	score_letters = analyze_ngrams(letter_data, analyze_letter, char_key_dict, key_objects, settings)
+	score_bigrams = analyze_ngrams(bigram_data, analyze_bigram, char_key_dict, key_objects, settings)
+	score::Float64 = (score_letters + score_bigrams) * 0.5
 	layout.score = score
-	# println(score)
+	# layout.finger_load = finger_load
 end
 
-
-function optimize_layout(layout::Layout, lang_prefs, iter::Int = 64, data_length::Int = 4096)
-	word_data = get_word_data(lang_prefs, data_length)
+function optimize_layout(layout::Layout, iter::Int, data_length::Int, key_objects::Tuple, settings)
+	word_data = get_word_data(settings.lang_prefs, data_length)
 	letter_data = get_ngram_data(word_data, 1)
 	bigram_data = get_ngram_data(word_data, 2)
-	analyze_layout(layout, letter_data, bigram_data)
+	analyze_layout(layout, letter_data, bigram_data, key_objects, settings)
 	layouts = [layout]
 	last_best_layout = layout
 	for i in 1:iter
@@ -232,72 +203,90 @@ function optimize_layout(layout::Layout, lang_prefs, iter::Int = 64, data_length
 			new_layouts = []
 			while length(new_layouts) < 32
 				tmp_layout = deepcopy(layout)
-				swap_keys!(tmp_layout)  # TODO 
-				if layout.char_key_dict == tmp_layout.char_key_dict
+				swap_keys!(tmp_layout, settings.freq_keys_on_home_row)
+				if layout.layout_chars == tmp_layout.layout_chars
 					continue
 				end
-				analyze_layout(tmp_layout, letter_data, bigram_data)
+				analyze_layout(tmp_layout, letter_data, bigram_data, key_objects, settings)
 				push!(new_layouts, tmp_layout)
 			end
 			sort!(new_layouts, by = layout -> layout.score, rev = false)
-			new_layouts = discard_bad_layouts!(new_layouts, 16.0, 4.0)
+			new_layouts = discard_bad_layouts!(new_layouts, 64.0, 4.0)
 			append!(layouts, new_layouts)
-			# TODO possible dupli
 		end
-		# del layouts_copy
 		sort!(layouts, by = layout -> layout.score, rev = false)
-		layouts = discard_bad_layouts!(layouts, 64.0, 4.0)
-		# print(length(layouts))
-		
+		layouts = discard_bad_layouts!(layouts, 256.0, 4.0)
 		best_layout_so_far = layouts[1]
-		if best_layout_so_far.char_key_dict != last_best_layout.char_key_dict
-			# print("\n", end="")
-			# print(f"Iteration {str(i + 1).zfill(len(str(iter)))} / {iter}")
+		if best_layout_so_far.layout_chars != last_best_layout.layout_chars
 			println("Best layout so far:")
 			print_layout(best_layout_so_far)
 			println(best_layout_so_far.score)
+			# p = plot(best_layout_so_far.finger_load)
+			# gui(p)
 			# best_layout_so_far.print_stats()
 		end
 		last_best_layout = best_layout_so_far
 	end
+	# print_layout.(layouts)
 	# print("yes")
 end
 
 
+# using Plots
+
 
 function main()
 
-	chars = [
-		'b' 'p' 'l' 'd' 'g' 'f' 'u' 'j'
-		's' 't' 'n' 'r' 'a' 'e' 'i' 'o'
-		'v' 'm' 'h' 'c' 'z' 'y' 'w' 'k'
-	]
+	key_objects = (
+		Key(false, 4, 1),
+		Key(false, 3, 1),
+		Key(false, 2, 1),
+		Key(false, 1, 1),
+		Key(true,  1, 1),
+		Key(true,  2, 1),
+		Key(true,  3, 1),
+		Key(true,  4, 1),
+		Key(false, 4, 2),
+		Key(false, 3, 2),
+		Key(false, 2, 2),
+		Key(false, 1, 2),
+		Key(true,  1, 2),
+		Key(true,  2, 2),
+		Key(true,  3, 2),
+		Key(true,  4, 2),
+		Key(false, 4, 3),
+		Key(false, 3, 3),
+		Key(false, 2, 3),
+		Key(false, 1, 3),
+		Key(true,  1, 3),
+		Key(true,  2, 3),
+		Key(true,  3, 3),
+		Key(true,  4, 3))
 
 	layout_string = "
-	c w h m  g u p j
-	s r n t  a e i o
-	b v l d  f z y k
+	b f r m  u o p j
+	w s n t  i e h g
+	v c l d  y a k z
 	"
-	# chars = only.(split(layout_string))
 	chars = collect(join(split(layout_string)))
-	# println(chars)
-	chars = permutedims(reshape(chars, (8, 3)))
-	char_key_dict = Dict{Char, Key}()
+
+	layout = Layout(chars, Inf, Vector{Float64}(undef, 8))
 	
-	for (i, col) in enumerate(eachcol(chars))
-		hand = i <= 4 ? false : true
-		finger = i <= 4 ? 5 - i : i - 4
-		for (j, char) in enumerate(col)
-			row = 4 - j
-			char_key_dict[char] = Key(hand, finger, row)
-		end
-	end
-	
-	layout = Layout(char_key_dict, Inf)
-	# print(layout.char_key_dict)
-	
-	lang_prefs = Dict("en" => 0.7, "cs" => 0.3)
-	@time optimize_layout(layout, lang_prefs, 1, 4096)
+	settings = (
+		# lang_prefs = Dict("en" => 0.7, "cs" => 0.3),
+		lang_prefs = Dict("en" => 1.0),
+		finger_efforts = (1.2, 1.0, 1.5, 2.3),
+		sfb = 4.0,
+		inroll = 0.7,
+		outroll = 1.2,
+		alter = 0.75,
+		freq_keys_on_home_row = false,
+		not_home_row = 2.0,
+		enforce_balance = 1.0,  # TODO 
+		prefer_bottom_row = 0.0  # TODO 
+	)
+	@time optimize_layout(layout, 16, 4096, key_objects, settings)
+	# BUG duplicate "you" on line 8474 in "en" make it freak out
 
 end
 

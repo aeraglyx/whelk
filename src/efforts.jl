ad16251c928522cf25d03af3df13d23d0f4b34cb
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2025-12-05 06:08:56 +0100

    cleanup

--- src/efforts.jl
+++ src/efforts.jl
@@ -1,20 +1,19 @@
 function letter_effort(key::Key, cfg)::Float64
-	effort::Float64 = 1.0 / cfg.finger_strengths[key.finger]
+	effort = 1.0 / cfg.finger_strengths[key.finger]
 
 	x = key.offset.x
 	y = key.offset.y
 	displacement = cfg.lateral * x * x + y * y
 
 	effort *= (1.0 + displacement * cfg.prefer_home)
-
 	return effort
 end
 
 
 function get_dexterity_integrated(dexterity_map)::Vector{Float64}
-	accum::Float64 = 0.0
+	accum = 0.0
 	dexterity_integrated = Vector{Float64}(undef, length(dexterity_map))
-	for i in 1:length(dexterity_map)
+	for i in eachindex(dexterity_map)
 		dexterity_integrated[i] = accum
 		if i < 4
 			accum += (dexterity_map[i] + dexterity_map[i+1]) * 0.5
@@ -25,40 +24,30 @@ end
 
 
 function bigram_effort(key_1::Key, key_2::Key, cfg)::Float64
-
-	effort::Float64 = 1.0
-
-	# TODO: put this outside
-	dexterity_map = cfg.finger_dexterity
-	dexterity_integrated = get_dexterity_integrated(dexterity_map)
-
-	if key_1.hand == key_2.hand
-		finger_diff = key_2.finger - key_1.finger
-		bigram_dexterity = abs(dexterity_integrated[key_2.finger] - dexterity_integrated[key_1.finger])
-
-		x = key_2.offset.x - key_1.offset.x
-		y = key_2.offset.y - key_1.offset.y
-		displacement = cfg.lateral * x * x + y * y
-
-		if finger_diff == 0
-			effort *= 1.0 + cfg.sfb * displacement / dexterity_map[key_1.finger]
-
-			y < 0 && (effort *= cfg.top_to_bottom)
-			y > 0 && (effort /= cfg.top_to_bottom)
-		else
-			dependence = 0.5 ^ (bigram_dexterity * cfg.independence)
-
-			if y != 0
-				effort *= 1.0 + cfg.scissor * displacement * dependence
-			end
-
-			finger_diff < 0 && (effort *= cfg.inroll)
-			finger_diff > 0 && (effort /= cfg.inroll)
-		end
-
-		effort *= cfg.one_hand
+	effort = 1.0
+	if key_1.hand != key_2.hand
+		return effort
 	end
 
+	finger_diff = key_2.finger - key_1.finger
+	x = key_2.offset.x - key_1.offset.x
+	y = key_2.offset.y - key_1.offset.y
+	displacement = cfg.lateral * x*x + y*y
+
+	if finger_diff == 0
+		effort *= 1.0 + cfg.sfb * displacement / cfg.finger_dexterity[key_1.finger]
+		y < 0 && (effort *= cfg.top_to_bottom)
+		y > 0 && (effort /= cfg.top_to_bottom)
+	else
+		dexterity_integrated = get_dexterity_integrated(cfg.finger_dexterity)
+		bigram_dexterity = abs(dexterity_integrated[key_2.finger] - dexterity_integrated[key_1.finger])
+		dependence = 0.5 ^ (bigram_dexterity * cfg.independence)
+		y != 0 && (effort *= 1.0 + cfg.scissor * displacement * dependence)
+		finger_diff < 0 && (effort *= cfg.inroll)
+		finger_diff > 0 && (effort /= cfg.inroll)
+	end
+
+	effort *= cfg.one_hand
 	return effort
 end
 
@@ -90,7 +79,6 @@ end
 function get_ngram_efforts(key_objects, cfg)
 	letter_efforts = get_letter_efforts(key_objects, cfg)
 	bigram_efforts = get_bigram_efforts(key_objects, cfg)
-
 	ngram_efforts = (letter_efforts, bigram_efforts)
 	return ngram_efforts
 end
